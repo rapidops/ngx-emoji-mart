66ede93680f17aef3b4882e1d95d3d63a29ede10
dhoop <dhoop.soni@rapidops.com>
2021-03-04 17:08:01 +0530

    added services

--- src/lib/picker/picker.module.ts
+++ src/lib/picker/picker.module.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { EmojiModule } from '@ctrl/ngx-emoji-mart/ngx-emoji';
+import { EmojiModule, EmojiService } from '@ctrl/ngx-emoji-mart/ngx-emoji';
 import { AnchorsComponent } from './anchors.component';
 import { CategoryComponent } from './category.component';
 import { PickerComponent } from './picker.component';
 import { PreviewComponent } from './preview.component';
 import { SearchComponent } from './search.component';
 import { SkinComponent } from './skins.component';
+import { EmojiFrequentlyService } from './emoji-frequently.service';
 
 @NgModule({
   imports: [CommonModule, FormsModule, EmojiModule],
@@ -28,5 +29,13 @@ import { SkinComponent } from './skins.component';
     PreviewComponent,
     SkinComponent,
   ],
+  providers: [
+    EmojiService,
+    EmojiFrequentlyService
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+    NO_ERRORS_SCHEMA,
+  ]
 })
 export class PickerModule {}
